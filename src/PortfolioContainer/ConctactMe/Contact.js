872398069e02a636e6dd8232bec017e63934659c
Federico Lencina <federiconahuellencina@gmail.com>
2022-02-04 17:45:10 -0300

    se agrega funcionalidad al formulario para evitar enviar el mail sin completar los cambios

--- src/PortfolioContainer/ConctactMe/Contact.js
+++ src/PortfolioContainer/ConctactMe/Contact.js
@@ -1,93 +1,200 @@
-import React from 'react';
-import '../../Styles/Contact.css';
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React from "react";
+import { useState } from "react";
+import "../../Styles/Contact.css";
+import { useRef, useEffect } from "react";
+import emailjs from "@emailjs/browser";
 
 
 export default function Contact() {
-    const formRef=useRef();
+  const formRef = useRef();
+  const initialValues = { username: "", subject: "", email: "", message: "" };
+  const [ formValues, setFormValues ] = useState(initialValues);
+  const [ formErrors, setFormErrors ] = useState({});
+  const [ isSubmit, setIsSubmit ] = useState(false);
+  const [ sendMessage, setSendMessage ] = useState(false);
+  
+  const closeMessageSend = () =>{
+    setSendMessage(false);
+  }
 
-    const handlerSubmit= (e)=>{
-        e.preventDefault();
-        emailjs.sendForm(
-            'service_xoxklpd',
-            'template_jqz9op4',
-            formRef.current,
-            'user_uqQfdg8XpW3ZuyDWUlhcj')
-        .then((result) => {
-          console.log(result.text);
-        }, (error) => {
-          console.log(error.text);
-        });
-    };
+  const handlerChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
     
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors, formValues, isSubmit]);
+
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Name is required!";
+    }
+    if (!values.subject) {
+      errors.subject = "Subject is required!";
+    }
+    if (!values.message) {
+      errors.message = "Message is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    return errors;
+  };
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+    setSendMessage(true);
+    
+    if(e.target.name.value !== "" && e.target.email.value !== ""&& e.target.subject.value !== "" && e.target.message.value)
+      emailjs
+      .sendForm(
+        "service_xoxklpd",
+        "template_jqz9op4",
+        formRef.current,
+        "user_uqQfdg8XpW3ZuyDWUlhcj"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setFormValues({
+            username:"",
+            subject:"",
+            email:"",
+            message:""
+          })
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      console.log(Object.keys(formErrors))
+    
+    
+};
+
   return (
-            <div id="contact"> 
-                    <div className="container py-5">
-                        <div className="row">
-                            <div className="col-12 col-sm-6 col-lg-6 box"> 
-                                <div className="text-dark title">
-                                        <h2>Contact Me</h2>
-                                </div>
-                                <div className="contactUnderline"></div>
-                                <div className="formContainer">
-                                    <form ref={formRef}  onSubmit={handlerSubmit} className="d-flex flex-column justify-content-center ">
-                                        <input type="text" placeholder="Name" name="user-name" className="form-control bg-transparent "></input>
-                                        <input type="text" placeholder="Subject" name="user-subject" className="form-control bg-transparent"></input>
-                                        <input type="text" placeholder="Email" name="user-email" className="form-control bg-transparent"></input>
-                                        <textarea rows="4" placeholder="Your Message" className="form-control bg-transparent" ></textarea>
-                                        <div className="d-flex justify-content-center w-100">
-                                            <button className="btn w-50">Send Message</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div className="col-12 col-sm-6 col-lg-6 pt-5 ">
-                                <div className="container">
-                                    <div className="contactsTitle">
-                                        <div className="text-dark">
-                                                <h2>Get in touch</h2>
-                                        </div>
-                                        <div className="getTouchUnderline "></div>
-                                        <p className="mb-5 getInTouchP">Always available to get in touch and bring my services. Feel free to contact me.</p>
-                                        <div className="container">
-                                            <div className="d-flex justify-content-evenly my-3 mediaContact">
-                                                <div className="contact w-25 text-center">
-                                                    <a href="https://www.linkedin.com/in/federico-lencina/" target="blank">
-                                                        <i className="fab fa-linkedin"></i>
-                                                    </a>
-                                                </div>
-                                                <div className="w-75">
-                                                    <p className="contactP">Linkedin</p>
-                                                </div>
-                                            </div>
-                                            <div className="d-flex justify-content-evenly my-3 mediaContact">
-                                                <div className="contact w-25 text-center">
-                                                    <a href="https://wa.link/520qgd" target="blank">
-                                                        <i class="fab fa-whatsapp"></i>
-                                                    </a>
-                                                </div>
-                                                <div className="w-75">
-                                                    <p className="contactP">Phone</p>
-                                                </div>
-                                            </div>
-                                            <div className="d-flex justify-content-evenly my-3 mediaContact">
-                                                <div className="contact w-25 text-center">
-                                                    <a href="mailto:federiconahuellencina@gmail.com" target="blank">
-                                                        <i className="fab fa-google"></i>
-                                                    </a>
-                                                </div>
-                                                <div className="w-75">
-                                                    <p className="contactP">Email</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                            </div> 
-                            </div>
-                    </div>
+    <div id="contact">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-12 col-sm-6 col-lg-6 box">
+            <div className="text-dark title">
+              <h2>Contact Me</h2>
             </div>
-    );
+            <div className="contactUnderline"></div>
+            <div className="formContainer">
+              {validate(formValues)===null}
+              <form
+                ref={formRef}
+                onSubmit={handlerSubmit}
+                className="d-flex flex-column justify-content-center "
+              >
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="username"
+                  className="form-control bg-transparent "
+                  value={formValues.username}
+                  onChange={handlerChange}
+                ></input>
+                <p>{formErrors.username}</p>
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  name="subject"
+                  className="form-control bg-transparent"
+                  value={formValues.subject}
+                  onChange={handlerChange}
+                ></input>
+                <p>{formErrors.subject}</p>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  className="form-control bg-transparent"
+                  value={formValues.email}
+                  onChange={handlerChange}
+                ></input>
+                <p>{formErrors.email}</p>
+                <textarea
+                  rows="4"
+                  placeholder="Your Message"
+                  name="message"
+                  className="form-control bg-transparent"
+                  value={formValues.message}
+                  onChange={handlerChange}
+                ></textarea>
+                <p className="text-danger">{formErrors.message}</p>
+                <div className="d-flex justify-content-center w-100">
+                  <button className="btn w-50">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-lg-6 pt-5 ">
+            <div className="container">
+              <div className="contactsTitle">
+                <div className="text-dark">
+                  <h2>Get in touch</h2>
+                </div>
+                <div className="getTouchUnderline "></div>
+                <p className="mb-5 getInTouchP">
+                  Always available to get in touch and bring my services. Feel
+                  free to contact me.
+                </p>
+                <div className="container">
+                  <div className="d-flex justify-content-evenly my-3 mediaContact">
+                    <div className="contact w-25 text-center">
+                      <a
+                        href="https://www.linkedin.com/in/federico-lencina/"
+                        target="blank"
+                      >
+                        <i className="fab fa-linkedin"></i>
+                      </a>
+                    </div>
+                    <div className="w-75">
+                      <p className="contactP">Linkedin</p>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-evenly my-3 mediaContact">
+                    <div className="contact w-25 text-center">
+                      <a href="https://wa.link/520qgd" target="blank">
+                        <i class="fab fa-whatsapp"></i>
+                      </a>
+                    </div>
+                    <div className="w-75">
+                      <p className="contactP">Phone</p>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-evenly my-3 mediaContact">
+                    <div className="contact w-25 text-center">
+                      <a
+                        href="mailto:federiconahuellencina@gmail.com"
+                        target="blank"
+                      >
+                        <i className="fab fa-google"></i>
+                      </a>
+                    </div>
+                    <div className="w-75">
+                      <p className="contactP">Email</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/PortfolioContainer/NavBar/Navbar.js
+++ src/PortfolioContainer/NavBar/Navbar.js
@@ -3,22 +3,22 @@ import './../../Styles/NavBar.css';
 
 export default function Navbar() {
     return (
-       <div class= "barraNavegacion bg-dark">
+       <div className= "barraNavegacion bg-dark">
         <div className="bg-light">
-            <div class= "barraNavegacion">
+            <div className= "barraNavegacion">
 
                 <nav class="navbar navbar-expand-lg navbar-light pb-3">
-                    <div class="container-fluid">
-                        <p class="navbar-brand w-50 text-center text-dark" id="nombre"><span class="text-danger">{"<"}</span>Federico Lencina<span class="text-danger fs-3">{"/>"}</span></p>
-                        <button class="navbar-toggler btn-light border border-dark border-4 bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                    <div className="container-fluid">
+                        <p className="navbar-brand w-50 text-center text-dark" id="nombre"><span class="text-danger">{"<"}</span>Federico Lencina<span class="text-danger fs-3">{"/>"}</span></p>
+                        <button className="navbar-toggler btn-light border border-dark border-4 bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav w-100 d-flex justify-content-around px-5 me-5">
-                            <a class="nav-link text-dark fs-4" href="#about">About</a>
-                            <a class="nav-link text-dark fs-4" href="#skills">Skills</a>
-                            <a class="nav-link text-dark fs-4" href="#proyects">Proyects</a>
-                            <a class="nav-link text-dark fs-4" href="#contact">Contact</a>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav w-100 d-flex justify-content-around px-5 me-5">
+                            <a className="nav-link text-dark fs-4" href="#about">About</a>
+                            <a className="nav-link text-dark fs-4" href="#skills">Skills</a>
+                            <a className="nav-link text-dark fs-4" href="#proyects">Proyects</a>
+                            <a className="nav-link text-dark fs-4" href="#contact">Contact</a>
                             
                             </div>
                         </div>
